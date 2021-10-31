385e866685ec003df16b9416621ede9aee267bd8
ulungp <ulung.prayitno93@gmail.com>
2021-10-31 18:11:15 +0700

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Navbar />
+  <div class="px-4 py-20">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import Navbar from './components/navbar.vue'
+
+  export default {
+    components: {
+      Navbar
+    }
+
+  }
+</script>
---
+++ src/components/navbar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex items-center sm:justify-start justify-between bg-gray-400 p-4 fixed z-1 top-0 w-full h-18 pl-8">
+    <div class="text-4xl font-extrabold sm:mr-8">myPokedex</div>
+    <div class="text-1xl font-medium">Favourite</div>
+  </div>
+</template>
